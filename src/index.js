d3adbb77fb1b2f7ae4fac4be1f957b38db97877c
Tro-fish <dreamnawas@naver.com>
2022-01-23 03:43:47 +0900

    apollo ready!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
+import { render } from "react-dom";
+import App from "./components/App";
+import React from "react";
 
-
-ReactDOM.render(
+render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
---
+++ src/routes/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error } = useQuery(GET_MOVIES);
+ 
+  return <h1>Home</h1>;
+};
